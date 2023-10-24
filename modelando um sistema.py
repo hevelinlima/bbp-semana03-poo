1b7168487afa1df47d8c627d9678545f9f853576
Hévelin Lima <123334849+hevelinlima@users.noreply.github.com>
2023-10-23 22:39:02 -0300

    Update modelando um sistema.py

--- modelando um sistema.py
+++ modelando um sistema.py
@@ -3,7 +3,7 @@
 info_conta=ContaCorrente()
 
 info_conta.adc_titular('Maria José')
-info_conta.adc_titular('Sávio lindo')
-info_conta.adc_titular('Hemilly gostosa')
+info_conta.adc_titular('Sávio')
+info_conta.adc_titular('Hemilly')
 
 info_conta.listar_titulares()"""
